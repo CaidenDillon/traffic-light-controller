736d6eff4348a6e1d63df0566f360c9cc51f734e
Caiden Dillon <caidenjdillon@icloud.com>
2026-02-20 16:20:08 -0500

    Refactor traffic light controller to use C++ features

--- traffic-light-controller.c
+++ traffic-light-controller.c
@@ -1,81 +1,69 @@
-#include <stdio.h>
-#include <windows.h>
+#include <chrono>
+#include <iostream>
+#include <thread>
 
-typedef enum
-{
+enum class Phase {
     NS_GREEN,
     NS_YELLOW,
     EW_GREEN,
     EW_YELLOW
-} TrafficLightState;
+};
 
-typedef struct
-{
-    TrafficLightState state;
-    int timer;
-} TrafficLight;
+struct TrafficLight {
+    Phase phase{Phase::NS_GREEN};
+    int timer{0}; // seconds elapsed in current phase
+};
 
-#define GREEN_DURATION 5
-#define YELLOW_DURATION 2
-#define TICK_MS 1000
+constexpr int GREEN_DURATION  = 5;
+constexpr int YELLOW_DURATION = 2;
 
-void simulation(TrafficLight *trafficlight)
-{
-    while (1)
-    {
-        switch (trafficlight->state)
-        {
-        case NS_GREEN:
-            printf("NS: GREEN, EW: RED\n");
-            trafficlight->timer++;
-            if (trafficlight->timer >= GREEN_DURATION)
-            {
-                trafficlight->state = NS_YELLOW;
-                trafficlight->timer = 0;
+void tick(TrafficLight& tl) {
+    using std::cout;
+
+    switch (tl.phase) {
+        case Phase::NS_GREEN:
+            cout << "NS: GREEN, EW: RED\n";
+            ++tl.timer;
+            if (tl.timer >= GREEN_DURATION) {
+                tl.phase = Phase::NS_YELLOW;
+                tl.timer = 0;
             }
             break;
 
-        case NS_YELLOW:
-            printf("NS: YELLOW, EW: RED\n");
-            trafficlight->timer++;
-            if (trafficlight->timer >= YELLOW_DURATION)
-            {
-                trafficlight->state = EW_GREEN;
-                trafficlight->timer = 0;
+        case Phase::NS_YELLOW:
+            cout << "NS: YELLOW, EW: RED\n";
+            ++tl.timer;
+            if (tl.timer >= YELLOW_DURATION) {
+                tl.phase = Phase::EW_GREEN;
+                tl.timer = 0;
             }
             break;
 
-        case EW_GREEN:
-            printf("NS: RED, EW: GREEN\n");
-            trafficlight->timer++;
-            if (trafficlight->timer >= GREEN_DURATION)
-            {
-                trafficlight->state = EW_YELLOW;
-                trafficlight->timer = 0;
+        case Phase::EW_GREEN:
+            cout << "NS: RED, EW: GREEN\n";
+            ++tl.timer;
+            if (tl.timer >= GREEN_DURATION) {
+                tl.phase = Phase::EW_YELLOW;
+                tl.timer = 0;
             }
             break;
 
-        case EW_YELLOW:
-            printf("NS: RED, EW: YELLOW\n");
-            trafficlight->timer++;
-            if (trafficlight->timer >= YELLOW_DURATION)
-            {
-                trafficlight->state = NS_GREEN;
-                trafficlight->timer = 0;
+        case Phase::EW_YELLOW:
+            cout << "NS: RED, EW: YELLOW\n";
+            ++tl.timer;
+            if (tl.timer >= YELLOW_DURATION) {
+                tl.phase = Phase::NS_GREEN;
+                tl.timer = 0;
             }
             break;
-
-        default:
-            break;
-        }
-
-        Sleep(TICK_MS);
     }
 }
 
-int main(void)
-{
-    TrafficLight trafficlight = {NS_GREEN, 0};
-    simulation(&trafficlight);
-    return 0;
+int main() {
+    TrafficLight tl;
+
+    while (true) {
+        tick(tl);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 }
